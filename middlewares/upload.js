4acbbe6633559f3986aa3fd0425ef7d2ba8d280f
Dmytro Chushko <dmytro.chushko@gmail.com>
2022-10-15 01:34:21 +0300

    add updateAvatar controller

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,3 +1,23 @@
 const multer = require('multer');
 const path = require('path');
 
+const maxAvatarSize = 200000;
+const tempDir = path.join(__dirname, '../', 'temp');
+
+const multerConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  limits: {
+    filsize: maxAvatarSize,
+  },
+});
+
+const upload = multer({
+  storage: multerConfig,
+});
+
+module.exports = upload;
